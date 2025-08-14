bcfac07fe56baede015eabd1100453369194207a
Tamar Laufer <tamarlaufer@gmail.com>
2025-08-14 16:13:12 +0300

    start project and building basic navigation

---
+++ src/stackNavigation/BottomTabs.tsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import Home from '../screens/Home';
+import Favorites from '../screens/Favorites';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+
+export type TabsParamList = {
+  Home: undefined;
+  Favorites: undefined;
+};
+
+const Tab = createBottomTabNavigator<TabsParamList>();
+
+export default function BottomTabs() {
+  return (
+    <Tab.Navigator
+      screenOptions={({ route }) => ({
+        headerShown: false,
+        tabBarIcon: ({ focused, color, size }) => {
+          let iconName: string;
+
+          if (route.name === 'Home') {
+            iconName = focused ? 'home' : 'home-outline';
+          } else if (route.name === 'Favorites') {
+            iconName = focused ? 'heart' : 'heart-outline';
+          }
+
+          return <Ionicons name={iconName!} size={size} color={color} />;
+        },
+        tabBarActiveTintColor: '#007AFF',
+        tabBarInactiveTintColor: 'gray',
+      })}
+    >
+      <Tab.Screen name="Home" component={Home} options={{ title: 'בית' }} />
+      <Tab.Screen
+        name="Favorites"
+        component={Favorites}
+        options={{ title: 'מועדפים' }}
+      />
+    </Tab.Navigator>
+  );
+}
